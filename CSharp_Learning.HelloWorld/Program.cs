edcca27dc529c9a11dd7cd6f6b497c737c3116d7
ahmetcoder55 <244322943+ahmetcoder55@users.noreply.github.com>
2026-09-01 19:42:17 +0300

    Add project files.

---
+++ CSharp_Learning.HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+ *In C# programming language,
+ *the Console class is used to print "Hello World" to the console.
+ *The Write method of the Console class is used to print "Hello World" to the console. *
+*/
+
+Console.Write("Hello World");
