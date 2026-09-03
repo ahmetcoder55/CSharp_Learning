ed91ec483e026300cdfdaa9ce0f9a4c45d9b45e0
ahmetcoder55 <244322943+ahmetcoder55@users.noreply.github.com>
2026-09-03 19:34:39 +0300

    Practice was done on the topic of variables.

---
+++ CSharp_Learning.VariablesAndHelloWorldDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿//Cafe Registration Application
+
+string name;
+string surname;
+int age;
+
+Console.Title = "Cafe Registration Application";
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
+Console.WriteLine("Welcome to Cafe Registration Application");
+Console.WriteLine("Please register for the application.");
+Console.Write("Name:");
+name= Console.ReadLine();
+Console.Write("Surname:");
+surname= Console.ReadLine();
+Console.Write("Age:");
+age = Convert.ToInt32(Console.ReadLine());
+string welcomeMessage = $"Name:{name}\n Surname:{surname} \n Age:{age.ToString()}";
+Console.WriteLine("Welcome!");
+Console.WriteLine(welcomeMessage);
+Console.ReadKey();
